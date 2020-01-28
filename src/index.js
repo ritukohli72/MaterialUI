b571261982d4a840e54c0bbf841b8391748d2229
Ritu Kohli <ritukohli72@gmail.com>
2020-01-28 14:07:04 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Demo from './demo';
+import Demo from './demo.js';
 
 ReactDOM.render(<Demo />, document.querySelector('#root'));
